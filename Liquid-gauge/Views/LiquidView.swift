6a7e3d5d2408ca01349e10e8ba56fe00c686beee
Anas AIT ALI <aitali.anas@gmail.com>
2014-12-02 14:56:04 +0100

    Initial structure

---
+++ Liquid-gauge/Views/LiquidView.swift
@@ -0,0 +1,21 @@
+//
+//  LiquidView.swift
+//  Liquid-gaugeDemo
+//
+//  Created by Anas Ait Ali on 02/12/14.
+//  Copyright (c) 2014 thanbeth. All rights reserved.
+//
+
+import UIKit
+
+class LiquidView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
