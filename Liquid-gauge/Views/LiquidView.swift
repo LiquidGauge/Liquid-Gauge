bbaa19c5ea846adbcb97b6f6f945e3fbd5aa7498
Benjamin BUSINARO <benjamin.businaro@etixgroup.com>
2014-12-18 11:08:42 +0100

    Adding accelerometer

--- Liquid-gauge/Views/LiquidView.swift
+++ Liquid-gauge/Views/LiquidView.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreMotion
 
 class LiquidView: UIView {
     
@@ -20,16 +21,22 @@ class LiquidView: UIView {
     
     var label = UILabel(frame: CGRectMake(30, 30, 100, 30))
     
+    var motionManager:CMMotionManager = CMMotionManager()
+    
     func initialize() {
+        
         if ((timer) != nil) {
             timer!.invalidate()
         }
         timer = NSTimer.scheduledTimerWithTimeInterval(0.025, target: self, selector: "update", userInfo: nil, repeats: true)
-//        NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "updateProgress", userInfo: nil, repeats: true)
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.06, target: self, selector: "calcAngleConstant", userInfo: nil, repeats: true)
+        //        NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "updateProgress", userInfo: nil, repeats: true)
         pourcentage = 68
         label.text = "\(pourcentage)%"
         self.addSubview(label)
         self.setNeedsDisplay()
+        println("🌺")
+        startMotionDetect()
     }
     
     override func awakeFromNib() {
@@ -67,7 +74,39 @@ class LiquidView: UIView {
         
     }
     
+    var accelerometer:CMAccelerometerData? = nil
+    
+    func startMotionDetect() {
+        self.motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue(), withHandler:{(accelerometerData :     CMAccelerometerData!, error : NSError!) in
+           
+            self.accelerometer = accelerometerData
+            
+        })
+    }
+    
+    var constant:Double = 0.0
+    
+    func calcAngleConstant() {
+        if (accelerometer != nil) {
+            
+            var multiplier = 1.0
+            if (accelerometer!.acceleration.x < 0) {
+                multiplier = -1.0
+            }
+            
+            constant = abs(accelerometer!.acceleration.y) * 100
+            var intConstant = Int(constant)
+            constant =  (1 - (Double(intConstant) / 10))  * multiplier
+            
+            if (Int(round(abs(accelerometer!.acceleration.z))) == 1) {
+                constant = 0.0
+            }
+        }
+    }
+    
     func drawWaves(rect: CGRect, context: CGContextRef, colorSpace: CGColorSpaceRef) {
+        
+        
         
         let marginLeft: Float = 0
         let marginRight: Float = 0
@@ -83,7 +122,6 @@ class LiquidView: UIView {
         var maxAmplitude: Float = vPosition - Float(2 * _lineWidth)
         var normedAmplitude: Float = _amplitude
         
-        
         UIColor.blueColor().colorWithAlphaComponent(0.5).set()
         
         var curve : CGMutablePathRef = CGPathCreateMutable()
@@ -91,12 +129,18 @@ class LiquidView: UIView {
         
         var lastX : CGFloat = 0
         var lastY : CGFloat = self.bounds.height / 2.0
+        
+        let midPoint = ((width+_density) / 2)
+        
         for (var x:Float = 0;x<width+_density; x+=_density) {
+          
+            var pute = (Double(midPoint) - Double(x)) * Double(constant)
+            
             CGContextMoveToPoint(context, lastX, lastY)
             var scaling: Float = -pow(1/mid*(x-mid),2)+1
             normedAmplitude = 0.05
             
-            var y: Float = scaling * maxAmplitude * normedAmplitude * sinf(2.0 * Float(M_PI) * (x / width) * _frequency + _phase) + vPosition
+            var y: Float = scaling * maxAmplitude * normedAmplitude * sinf(2.0 * Float(M_PI) * (x / width) * _frequency + _phase) + vPosition + Float(pute)
             
             CGPathAddLineToPoint(curve, nil, CGFloat(x+marginLeft), CGFloat(y))
             let location: CGFloat = CGFloat(x / (width+_density))
@@ -111,5 +155,6 @@ class LiquidView: UIView {
         CGContextAddPath(context, curve)
         CGContextFillPath(context)
     }
+   
 }
 
