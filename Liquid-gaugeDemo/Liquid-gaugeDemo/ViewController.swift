9481a7ff1a9b575a95e57764da736f3d940ca8ba
Benjamin BUSINARO <benjamin.businaro@etixgroup.com>
2014-12-18 14:03:18 +0100

    Upgrade UI

--- Liquid-gaugeDemo/Liquid-gaugeDemo/ViewController.swift
+++ Liquid-gaugeDemo/Liquid-gaugeDemo/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var liquidView: LiquidView!
+    @IBOutlet weak var textValue: UILabel!
+    @IBAction func valueChanged(sender: AnyObject) {
+        var slider = sender as UISlider
+        liquidView.pourcentage = round(slider.value * 100)
+        textValue.text = "\(liquidView.pourcentage) %"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
