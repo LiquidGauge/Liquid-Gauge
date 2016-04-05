a8734fc54f247e12296faec8b249617a0b156049
letusfly85 <letusfly95@gmail.com>
2016-04-06 00:21:43 +0900

    modify for ios ~> 8.0

---
+++ LiquidGauge/LiquidGauge.h
@@ -0,0 +1,19 @@
+//
+//  LiquidGauge.h
+//  LiquidGauge
+//
+//  Created by 和田 俊輔 on 2016/04/05.
+//  Copyright © 2016年 org.triplew. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for LiquidGauge.
+FOUNDATION_EXPORT double LiquidGaugeVersionNumber;
+
+//! Project version string for LiquidGauge.
+FOUNDATION_EXPORT const unsigned char LiquidGaugeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LiquidGauge/PublicHeader.h>
+
+
